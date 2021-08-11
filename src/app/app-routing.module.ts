e21c1e9418be15081a24bd409b31d7fd472339f6
Satyam <satyamganta@gmail.com>
2021-08-11 18:22:10 +0530

    Basic Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogsComponent } from './pages/catalogs/catalogs.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
+import { CoursesComponent } from './pages/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'courses', component: CoursesComponent },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'certificates', component: CertificatesComponent },
+  { path: 'catalogs', component: CatalogsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { MainNavComponent } from './layout/main-nav/main-nav.component';
 import { SubNavComponent } from './layout/sub-nav/sub-nav.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
+import { CatalogsComponent } from './pages/catalogs/catalogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     MainNavComponent,
-    SubNavComponent
+    SubNavComponent,
+    CoursesComponent,
+    FooterComponent,
+    CertificatesComponent,
+    CatalogsComponent
   ],
   imports: [
     BrowserModule,
